e11b5fb22e1a1569f0aac8d4d3f7406aedd0364c
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-08-05 18:34:15 +0530

    Project setup Completed

--- recipe-maker/src/main.jsx
+++ recipe-maker/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/home.jsx'
+import Home from './pages/home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
