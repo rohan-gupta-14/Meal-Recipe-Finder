a8b57ed01d996bdfb8b89007ff22f35e08b8b064
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-08-12 23:06:33 +0530

    Hero section

--- recipe-maker/src/components/HeroSection.jsx
+++ recipe-maker/src/components/HeroSection.jsx
@@ -4,10 +4,17 @@ import '../css/herosection.css';
 import heroImg from '../assets/Hero.jpg'; 
 const HeroSection = () => {
   return (
-    <div className='hero-container'>
-        <img src={heroImg} alt="Hero" className="hero-image" />
+    <div 
+      className="hero-container" 
+      style={{ backgroundImage: `url(${heroImg})` }}
+    >
+      <div className="hero-content">
+        <h1>Find Your Perfect Recipe </h1>
+        <button className="hero-btn">Search Recipes</button>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default HeroSection
