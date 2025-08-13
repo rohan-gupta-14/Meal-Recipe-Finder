8ee81d802af5d2c855e0eedd6234b7b6bf7429b7
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-08-13 17:36:49 +0530

    Completed

--- recipe-maker/src/components/SearchBar.jsx
+++ recipe-maker/src/components/SearchBar.jsx
@@ -1,29 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../css/searchBar.css';
-const SearchBar = () => {
+
+const SearchBar = ({ recipes, setRecipes }) => {
+    const [query, setQuery] = useState('');
+
+    const handleSearch = async () => {
+        if (!query.trim()) return;
+        try {
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+            const data = await res.json();
+            setRecipes(data.meals || []);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <div>
-
-            <nav class="navbar">
-                <div class="navbar-left">
-                    <i class="fa-solid fa-utensils logo"></i>
-                    <span class="brand">Recipe Finder</span>
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <i className="fa-solid fa-utensils logo"></i>
+                    <span className="brand">Recipe Finder</span>
                 </div>
 
-                <ul class="navbar-links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Recipe List</a></li>
-                    <li><a href="#">Saved Recipes</a></li>
+                <ul className="navbar-links">
+                    <li>
+                        <a href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setQuery('');
+                                setRecipes([]);
+                            }}>
+                            Home
+                        </a>
+                    </li>
+
                 </ul>
 
-                <div class="navbar-search">
-                    <input type="text" placeholder="Search recipes..." />
-                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                <div className="navbar-search">
+                    <input
+                        type="text"
+                        placeholder="Search recipes..."
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <button onClick={handleSearch}>
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
                 </div>
             </nav>
 
+            {/* Recipe Results */}
+            {recipes.length > 0 && (
+                <div className="recipe-results">
+                    {recipes.map((recipe) => (
+                        <div key={recipe.idMeal} className="recipe-card">
+                            <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipe-img" />
+                            <div className="recipe-info">
+                                <h3>{recipe.strMeal}</h3>
+                                <div className="recipe-text">
+                                    {recipe.strInstructions}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default SearchBar
+export default SearchBar;
--- recipe-maker/src/pages/home.jsx
+++ recipe-maker/src/pages/home.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import "../css/home.css"
-import SearchBar from '../components/SearchBar'
-import HeroSection from '../components/HeroSection'
+import React, { useState } from 'react';
+import "../css/home.css";
+import SearchBar from '../components/SearchBar';
+import HeroSection from '../components/HeroSection';
 
-const home = () => {
+const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+
   return (
     <div>
-      <SearchBar />
-      <HeroSection/>
+      <SearchBar recipes={recipes} setRecipes={setRecipes} />
+      {recipes.length === 0 && <HeroSection />}
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default Home;
